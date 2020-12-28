aebb2a2cba23600d3b0d63c41f867a6b2e82bde9
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-12-28 22:30:33 +0530

    Updates

---
+++ precache-manifest.8af4f0781422d267533f89486766a14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dc1e7a82cf53059c74274dda875534",
+    "url": "/Caller/index.html"
+  },
+  {
+    "revision": "86d978cdc8c7c1be38a1",
+    "url": "/Caller/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "921ea3c164be71e653a2",
+    "url": "/Caller/static/js/2.aaa65b5a.chunk.js"
+  },
+  {
+    "revision": "35c88316aac44c45429e3a30910df147",
+    "url": "/Caller/static/js/2.aaa65b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d978cdc8c7c1be38a1",
+    "url": "/Caller/static/js/main.bfba14fd.chunk.js"
+  },
+  {
+    "revision": "6b77667dd794ffe05e47",
+    "url": "/Caller/static/js/runtime-main.0a8bbdf2.js"
+  }
+]);
