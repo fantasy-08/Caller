331a183717741f44db11846e298fc6d5c55a5193
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2021-01-17 14:13:54 +0530

    yarn removed POSRT

--- server.js
+++ server.js
@@ -60,4 +60,6 @@ io.on("connection", (socket) => {
 	});
 });
 app.use('/Caller', express.static('./build'));
-server.listen(process.env.PORT, () => console.log("server is running on port 8000"));
+server.listen(process.env.PORT || 8000, () =>
+	console.log("server is running on port 8000")
+);
